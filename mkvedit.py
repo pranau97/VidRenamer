6627cfd45c3192565af08ac42c5fde2994e2bd3a
Pranau Kumar <pranau97@gmail.com>
2018-08-13 02:02:46 +0530

    Add comments. Fix some bugs

--- mkvedit.py
+++ mkvedit.py
@@ -14,6 +14,8 @@ import logging
 import subprocess
 from glob import glob
 from file import File
+
+# Configure the logger object and set logging level
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
@@ -24,11 +26,13 @@ def clrscr():
 
 
 def process_individual(mkv_list):
-    # Method to process and set values to a file one by one.
+    # This method takes all the mkv files in a given directory
+    # and gives the user the option to edit them one by one.
 
     # List to store all the video file objects
     videos = []
 
+    # Instantiate objects to store the video data
     for i in mkv_list:
         videos.append(File(i))
 
@@ -41,74 +45,111 @@ def process_individual(mkv_list):
     # List to store all the modified videos
     updated_videos = []
 
+    # Accept user inputs for the files
     for video in videos:
         while True:
             log.info("Processing file no. " + str(process_count))
 
+            # Print file info
             print("File {0} of {1}".format(
                 process_count, total))
             print("Path: ", video.current_path)
             print("Filename: ", video.current_filename)
             print("MKV segment title: ", video.current_metadata_title)
 
+            # Accept user input for the new video title
             temp = input(
                 "Enter the new MKV segment title "
                 "(Press ENTER to skip, \\ to copy filename) ")
+            # Process the input
+            # If the input is empty, do nothing
             if not temp:
-                temp = None
+                pass
+            # If the input is a '\', copy the filename without the extension
+            # to the title field
             elif temp == '\\':
+                video.set_metadata_title = os.path.splitext(
+                    video.set_filename)[0]
                 updated_videos.append(video)
                 update_count += 1
+            # Else, set the user input as the value of the title field
             else:
                 video.set_metadata_title = temp
                 updated_videos.append(video)
                 update_count += 1
 
+            # Confirm whether user wants to proceed with the
+            # next files or quit
             process = input(
                 "Press r to redo, s to stop processing, e to exit, "
                 "ENTER to continue ")
+            # Process the input
+            # If the input is empty, go to the next file
             if not process:
                 pass
+            # If the input is r, redo the edit process for the current file
             elif process.lower() == 'r':
                 update_count -= 1
                 continue
+            # If the input is s, stop the process and proceed directly to
+            # applying the changes made so far
             elif process.lower() == 's':
                 stop_flag = True
+            # If the input is e, terminate the current edit job completely
             elif process.lower() == 'e':
                 log.info("Forced exit by user.")
                 return 0
+            # Do nothing and proceed to the next file, otherwise
+            else:
+                pass
 
             process_count += 1
             clrscr()
             break
 
+        # Stop the loop to proceed to applying the changes
         if stop_flag:
             break
         stop_flag = False
 
+    # Store the total number of edited videos
     total_updated = len(updated_videos)
+    # Display the queued videos to be modified
     print("About to apply changes to {0} videos.".format(
         total_updated))
     for video in updated_videos:
         print(video.current_filename, ": ")
         print(video.current_metadata_title, "->", video.set_metadata_title)
+    # Prompt for confirmation
     process = input("Continue? (y/n) ")
+    clrscr()
 
+    # If user agrees, apply changes
     if process.lower() in ["yes", "y"]:
+        # Apply changes made to the videos iteratively
         for video in updated_videos:
-            clrscr()
-            video.apply_changes()
-            log.info("Applied change for video - " + str(video.current_path))
+            status_code = video.apply_changes()
+            # Continue silently if there is no error reported
+            if not status_code == 1:
+                log.info("Applied change for video - " +
+                         str(video.current_path))
+            # Raise an error and stop further processing on error
+            else:
+                log.error("Error while applying changes to " +
+                          str(video.current_path))
+                return 1
+
+        # Confirm that changes have been applied
         print("Applied changes to {0} videos.".format(
             total_updated))
+    # Else, discard all changes and return
     else:
+        log.info("Changes discarded.")
         print("Changes discarded.")
 
-    if not input("Press ENTER to continue"):
-        return 0
-    else:
-        log.warning("Non-standard exit.")
-        return 0
+    # Prompt for user input to continue
+    input("Press ENTER to continue")
+    return 0
 
 
 def run(dirpath):
@@ -130,16 +171,28 @@ def run(dirpath):
     mkv_list = [y for x in os.walk(os.getcwd())
                 for y in glob(os.path.join(x[0], '*.mkv'))]
 
-    mode = input("Single mode or pattern mode? (s/p) ")
-    if mode.lower() in ["single", "s"]:
+    ''' Prompt user input to run the script in one of two currently supported
+        modes.
+        1 - Single mode. This mode looks for all mkv files in a given directory
+        and gives the user the option to edit them one by one.
+        2 - Pattern mode. This mode lets the user specify a pattern or rule
+        which will be used to edit the mkv files in the given directory.
+    '''
+    process = input("Single mode or pattern mode? (s/p) ")
+    # If single is selected, call process_individual()
+    if process.lower() in ["single", "s"]:
         status_code = process_individual(mkv_list)
-    elif mode.lower() in ["pattern", "p"]:
+    # If pattern is selected, call process_pattern()
+    elif process.lower() in ["pattern", "p"]:
         pass
+    # Else, raise an error for providing an invalid option
     else:
         log.error("Invalid processing option.")
         return 1
 
-    if status_code:
+    # Raise an error if the status code returned by any of the editing
+    # methods is non-zero
+    if status_code == 1:
         log.error("Error while processing files")
         return 1
     else:
@@ -147,21 +200,33 @@ def run(dirpath):
 
 
 if __name__ == "__main__":
+    # Add config for the argument parsing library
     parser = argparse.ArgumentParser(
         description="Batch edit MKV files's metadata")
+    # Add argument to directly launch the script with a path
+    # instead of interactively accepting it via user input
+    # TODO: Add more command line arguments to automate the script
+    # TODO: Add restart argument
     parser.add_argument(
         '-p',
         '--path',
         default=None,
         help='Enter the directory path to the file(s) to edit.')
+    # Parse the command line arguments
     args = parser.parse_args()
 
     while True:
         status_code = run(args.path)
-        args.path = None
-        if status_code:
-            print("Error encountered.")
+        # Raise an error on non-zero status code returned by run()
+        if status_code == 1:
+            print("The script encountered an error.")
+            log.error("Error encountered.")
 
+        # Reset the value for the next iteration so that the path
+        # can be accepted via user input
+        args.path = None
+
+        # Accept user input to restart the script to edit new files
         process = input("Restart? (y/n) ")
         if process.lower() in ['yes', 'y']:
             log.info("Restarting...")
